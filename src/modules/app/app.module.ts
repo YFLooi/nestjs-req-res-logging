15287712f480d1bead1d76b8a678492c050188c3
YF Looi <yihfoo@gmail.com>
2022-08-29 23:01:58 +0800

    Implement logger.middleware

---
+++ src/modules/app/app.module.ts
@@ -0,0 +1,23 @@
+import {
+  Module,
+  NestModule,
+  OnModuleInit,
+  RequestMethod,
+  MiddlewareConsumer,
+} from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { LoggerMiddleware } from '../../middleware/logger.middleware';
+
+@Module({
+  imports: [],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(LoggerMiddleware)
+      .forRoutes({ path: '*', method: RequestMethod.ALL });
+  }
+}
